df8e056eeb477df3c98c37059e5887fe5f8c103e
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2019-11-01 17:32:05 +0100

    Build production version

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['babel-polyfill', './lib/index.js'],
+  //entry: ['babel-polyfill', './lib/index.js'],
+  entry: {
+    vendor: ['babel-polyfill', 'react', 'react-dom', 'prop-types'],
+    app: ['./lib/index.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+      { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['react', 'env', 'stage-2'] } } }
     ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        }
+      }
+    }
   }
 };
